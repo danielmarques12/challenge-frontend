19a2621ddb78e8ef69bc50e096ce4b89ecb998e5
daniel <daniel.brz2009@gmail.com>
2020-10-12 02:53:34 -0300

    react

--- src/pages/Transcription/index.js
+++ src/pages/Transcription/index.js
@@ -1,42 +1,8 @@
 /* eslint-disable react/sort-comp */
 import React from 'react';
-import {
-  PDFViewer,
-  Page,
-  Text,
-  View,
-  Document,
-  StyleSheet,
-  pdf,
-} from '@react-pdf/renderer';
 import { withRouter } from 'react-router-dom';
 import jsPDF from 'jspdf';
 import api from '../../services/api';
-
-// const styles = StyleSheet.create({
-//   page: {
-//     flexDirection: 'row',
-//     backgroundColor: '#red',
-//   },
-//   section: {
-//     margin: 10,
-//     padding: 10,
-//     flexGrow: 1,
-//   },
-// });
-
-// const MyDocument = () => (
-//   <Document>
-//     <Page size="A4" style={styles.page}>
-//       <View style={styles.section}>
-//         <Text>Section #1</Text>
-//       </View>
-//       <View style={styles.section}>
-//         <Text>Section #2</Text>
-//       </View>
-//     </Page>
-//   </Document>
-// );
 
 async function documentPDF(blobDocument) {
   const data = new FormData();
